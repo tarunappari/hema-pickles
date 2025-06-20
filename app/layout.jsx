483ba398a3893060d23677df444d86d3e1d1c2a6
tarun <tarunappari23@gmail.com>
2025-06-20 21:13:22 +0530

    added lenis for smooth scrolling

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/_globals.scss";
+import SmoothScroll from "@/components/animations/SmoothScroll";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <SmoothScroll />
         {children}
       </body>
     </html>
